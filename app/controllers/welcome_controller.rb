8a7d0a3e85a9762d99f7c4e588a07f594dbedbdc
Salome <salomeranson@gmail.com>
2017-02-06 19:58:31 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = 'Louisiana'
+  	@countries = ['Chile','Italy','Turks and Caicos']
+  	@travel_pics = %w[chile.jpg Italy.jpg turks.jpg]
+  end
+
+  def about
+   @color = params[:color]
+   @size = params[:size].to_i
+  end
+end
